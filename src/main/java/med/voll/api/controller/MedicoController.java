21782d86c28cc7c23e59bd67135de321301189a1
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-07-14 06:11:06 -0300

    criando um DTO para enviar a lista de médicos com as informações necessárias.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,21 +2,21 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 
     @Autowired
-    private MedicoRepository repository;
+    private MedicoRepository repository;//acessar o banco de dados através desse objeto
 
     //400 algum campo recebido foi enviado errado
     //*método-http* + Mapping
@@ -25,5 +25,10 @@ public class MedicoController {
     @Transactional//metodo de escrita do spring
     public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
