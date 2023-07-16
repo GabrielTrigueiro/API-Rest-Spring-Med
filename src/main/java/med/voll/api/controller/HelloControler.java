89a2777d78ccba08b45833c72022d24365b6c65c
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-07-16 07:41:32 -0300

    crud para paciente

--- src/main/java/med/voll/api/controller/HelloControler.java
+++ src/main/java/med/voll/api/controller/HelloControler.java
@@ -1,16 +1,10 @@
 package med.voll.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.flywaydb.core.Flyway;
 
-@RestController
-@RequestMapping("/hello")//se cair req nessa url ativa o HelloControler
 public class HelloControler {
+    public static void main(String[] args) {
 
-    @GetMapping
-    public String olaMundo() {
-        return "hello spring oi";
     }
 
 }
