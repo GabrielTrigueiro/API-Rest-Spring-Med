420b7c60bc1b3833f647d13345949c24a1bf7cd1
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-07-17 07:38:21 -0300

    tratando erro 400 com body personalizado

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,13 +2,27 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice//anotation para classe que trata erro
 public class TratadorDeErros {
-    @ExceptionHandler(EntityNotFoundException.class)//404
+    @ExceptionHandler(EntityNotFoundException.class)//404 página não encontrada
     public ResponseEntity tratarErro404(){
         return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)//400 campo inválido
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+        //pegando os erros
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String menssagem){
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
